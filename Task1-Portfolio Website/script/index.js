703aa23d326cbecf65965500671c55fe47c324b4
Siddhesh Pawar <86412677+Siddheshp2424@users.noreply.github.com>
2023-03-01 15:26:21 +0530

    Add files via upload

---
+++ Task1-Portfolio Website/script/index.js
@@ -0,0 +1,10 @@
+let typed = new Typed(".typing", {
+    strings: [ "",
+      " Sidhesh Pawar",
+      " Web Devloper",
+      
+    ],
+    typeSpeed: 150,
+    backSpeed: 80,
+    loop: true,
+  });
